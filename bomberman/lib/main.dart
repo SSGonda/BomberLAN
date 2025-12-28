657b9b0660b99ddf3d3f4b3fbd7f05643fd6877e
Kourinx <anmagpantay@up.edu.ph>
2025-12-29 00:35:46 +0800

    updates

--- bomberman/lib/main.dart
+++ bomberman/lib/main.dart
@@ -10,6 +10,68 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 const String serverUrl = 'ws://127.0.0.1:15000'; // connection url
 const double tileSize = 40.0; // tile size
 
+abstract class GameGrid extends SpriteComponent {
+  GameGrid({required Sprite sprite, required Vector2 position})
+      : super(sprite: sprite, size: Vector2.all(tileSize), position: position);
+}
+
+class HardBlock extends GameGrid {
+  HardBlock(Sprite sprite, Vector2 position) : super(sprite: sprite, position: position);
+}
+
+class SoftBlock extends GameGrid {
+  SoftBlock(Sprite sprite, Vector2 position) : super(sprite: sprite, position: position);
+}
+
+class Floor extends GameGrid {
+  Floor(Sprite sprite, Vector2 position) : super(sprite: sprite, position: position);
+}
+
+class BombComponent extends GameGrid {
+  final String id;
+  BombComponent(this.id, Sprite sprite, Vector2 position)
+      : super(sprite: sprite, position: position);
+}
+
+class PowerupComponent extends GameGrid {
+  final String id;
+  PowerupComponent(this.id, Sprite sprite, Vector2 position)
+      : super(sprite: sprite, position: position);
+}
+
+class ExplosionComponent extends GameGrid {
+  final String id;
+  ExplosionComponent(this.id, Sprite sprite, Vector2 position)
+      : super(sprite: sprite, position: position);
+}
+
+class PlayerComponent extends SpriteComponent {
+  final int playerId;
+  
+  late TextComponent label;
+
+  PlayerComponent({
+    required this.playerId,
+    required Sprite sprite,
+    required Vector2 position,
+  }) : super(sprite: sprite, size: Vector2.all(tileSize * 0.8), position: position) {
+    anchor = Anchor.center; 
+    
+    label = TextComponent(
+      text: 'P${playerId + 1}',
+      textRenderer: TextPaint(
+        style: const TextStyle(fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+    );
+    label.position = Vector2(size.x / 2 - 5, -12);
+    add(label);
+  }
+
+  // move to new position
+  void updateTargetPosition(double serverX, double serverY) {
+    position = Vector2(serverX * tileSize, serverY * tileSize);
+  }
+}
 void main() {
   runApp(GameWidget(game: BombermanClient()));
 }
