8b897777bea261cc428a829cb124143e15744410
Kourinx <anmagpantay@up.edu.ph>
2026-01-02 18:40:00 +0800

    add sounds

--- bomberman/lib/game_client.dart
+++ bomberman/lib/game_client.dart
@@ -5,6 +5,7 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flame_audio/flame_audio.dart';
 
 import 'components/grid.dart';
 import 'components/player.dart';
@@ -29,6 +30,11 @@ class BombermanClient extends FlameGame with HasKeyboardHandlerComponents {
 
   static const double tileSize = 40.0;
 
+  final Set<String> _playedExplosionIds = {};
+  final Map<int, bool> _previousAliveState = {};
+  final Map<int, Map<String, num>> _previousPlayerStats = {};
+  bool _gameOverSoundPlayed = false;
+
   BombermanClient({required this.serverIp, required this.serverPort});
 
   @override
@@ -39,6 +45,15 @@ class BombermanClient extends FlameGame with HasKeyboardHandlerComponents {
       'bomb.png', 'explosion.png',
       'powerup_fire.png', 'powerup_bomb.png', 'powerup_speed.png'
     ]);
+
+    await FlameAudio.audioCache.loadAll([
+    'explosion.mp3',
+    'death.mp3',
+    'powerup.mp3',
+    'win.mp3',
+    'lose.mp3',
+    'draw.mp3',
+  ]);
 
     camera.viewfinder.anchor = Anchor.topLeft;
 
@@ -93,6 +108,72 @@ class BombermanClient extends FlameGame with HasKeyboardHandlerComponents {
   }
 
   void _updateGameState(Map<String, dynamic> state) {
+
+    // audio
+    final serverExplosions = List<dynamic>.from(state['explosions']);
+    final currentExplosionIds = <String>{};
+
+    for (var eData in serverExplosions) {
+      final id = eData['id'] as String? ?? "${eData['x']}_${eData['y']}_${eData['timePlaced']}";
+      
+      currentExplosionIds.add(id);
+
+      if (!_playedExplosionIds.contains(id)) {
+        FlameAudio.play('explosion.mp3');
+        _playedExplosionIds.add(id);
+      }
+    }
+    _playedExplosionIds.removeWhere((id) => !currentExplosionIds.contains(id));
+
+    final serverPlayers = List<dynamic>.from(state['players']);
+    
+    for (var pData in serverPlayers) {
+      final pid = pData['id'] as int;
+      final isAlive = pData['isAlive'] as bool;
+      
+      // death
+      if ((_previousAliveState[pid] ?? true) && !isAlive) {
+        FlameAudio.play('death.mp3');
+      }
+      _previousAliveState[pid] = isAlive;
+
+      // powerup pickup
+      final currentStats = {
+        'range': pData['bombRange'] as num,
+        'maxBombs': pData['maxBombs'] as num,
+        'speed': pData['speed'] as num,
+      };
+
+      if (_previousPlayerStats.containsKey(pid)) {
+        final prev = _previousPlayerStats[pid]!;
+        if (currentStats['range']! > prev['range']! || 
+            currentStats['maxBombs']! > prev['maxBombs']! ||
+            currentStats['speed']! > prev['speed']!) {
+          FlameAudio.play('powerup.mp3');
+        }
+      }
+      _previousPlayerStats[pid] = currentStats;
+    }
+
+    // win lose draw
+    final winner = state['winner'];
+    
+    if (winner != null && !_gameOverSoundPlayed && myPlayerId != null) {
+      _gameOverSoundPlayed = true;
+
+      if (winner == 'draw') {
+        FlameAudio.play('draw.mp3');
+      } else {
+        final winnerId = int.tryParse(winner.toString().replaceAll(RegExp(r'[^0-9]'), ''));
+        
+        if (winnerId == myPlayerId) {
+          FlameAudio.play('win.mp3');
+        } else {
+          FlameAudio.play('lose.mp3');
+        }
+      }
+    }
+
     // grid
     final gridData = List<List<dynamic>>.from(state['grid']);
     gridComponent.updateGrid(gridData);
@@ -104,7 +185,7 @@ class BombermanClient extends FlameGame with HasKeyboardHandlerComponents {
     }
 
     // players
-    final serverPlayers = List<dynamic>.from(state['players']);
+    // final serverPlayers = List<dynamic>.from(state['players']);
     final activeIds = <int>{};
     for (var pData in serverPlayers) {
       final pid = pData['id'] as int;
@@ -149,7 +230,7 @@ class BombermanClient extends FlameGame with HasKeyboardHandlerComponents {
     });
 
     // explosions
-    final serverExplosions = List<dynamic>.from(state['explosions']);
+    // final serverExplosions = List<dynamic>.from(state['explosions']);
     final activeExpKeys = <String>{};
     
     for (var eData in serverExplosions) {
